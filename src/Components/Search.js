32d61ec1b96c537a1f972110ace57865c1d6f455
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-10-10 05:09:35 +0600

    search input style complete

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -5,11 +5,12 @@ const Search = () => {
   const { query, setQuery, isError } = useGlobalContext();
   return (
     <>
-      <section className="search-section">
+      <section className="mx-auto text-center my-12">
         <h2>Cinema & Popcorn</h2>
         <form action="#" onSubmit={(e) => e.preventDefault()}>
           <div>
             <input
+              className="w-96 rounded-full border-none border-blue-400 px-6 py-3 text-2xl shadow-sm shadow-blue-500/50 outline-none mt-6"
               type="text"
               placeholder="search movie"
               value={query}
