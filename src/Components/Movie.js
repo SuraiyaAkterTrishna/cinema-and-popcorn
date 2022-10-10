a87628c0bf859c19e8f891846fbeee1683ff89d3
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-10-10 18:55:02 +0600

    project done

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -6,6 +6,9 @@ const imgUrl = "https://image.tmdb.org/t/p/original";
 
 const Movie = () => {
   const { movie, isLoading } = useGlobalContext();
+  const movieFilter = () => {
+    return [...new Set(movie.map((item) => item.release_date))];
+  };
   if (isLoading) {
     return <div className="loading">Loading....</div>;
   }
@@ -13,19 +16,38 @@ const Movie = () => {
   return (
     <>
       <section className="movie-page">
+        <div className="filter">
+          <input
+            className="dropdown"
+            type="checkbox"
+            id="dropdown"
+            name="dropdown"
+          />
+          <label className="for-dropdown" for="dropdown">
+            Select Year
+          </label>
+          <div className="section-dropdown">
+            {movieFilter().map((year) => (
+              <option className="link" value={year} key={year}>
+                {year}
+              </option>
+            ))}
+          </div>
+        </div>
         <div className="grid grid-4-col">
           {movie
             ? movie.map((data) => {
                 const { id, original_title, poster_path } = data;
-                const poster = (poster_path === null)? 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg':  imgUrl+poster_path;
+                const poster =
+                  poster_path === null
+                    ? "https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg"
+                    : imgUrl + poster_path;
 
                 return (
                   <NavLink to={`movie/${id}`} key={id}>
                     <div className="card">
                       <div className="card-info">
-                        <h2>
-                          {original_title}
-                        </h2>
+                        <h2>{original_title}</h2>
                         <img src={poster} alt="#" />
                       </div>
                     </div>
